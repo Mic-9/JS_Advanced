7bea413581bd63f9ff869ca6123d40a145ff4bea
Mic-9 <116760683+Mic-9@users.noreply.github.com>
2024-03-18 22:51:00 +0100

    frame base pt.2

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,20 @@
 let corpo = document.querySelector("main");
 corpo.setAttribute("id", "corpo");
 
-function NewsFrame() {
-  const frame = document.createElement("p");
+function NewsFrame(classe, div_a) {
+  const frm = document.createElement("div");
+  frm.className = classe;
+
+  const titolo = document.createElement("h3");
+  frm.appendChild(titolo);
   const testo = document.createTextNode("prova prova sa sa");
-  frame.appendChild(testo);
+  titolo.appendChild(testo);
+
+  const link = document.createElement("div");
+  link.className = div_a;
+  frm.appendChild(link);
 
   const corpo = document.getElementById("corpo");
-  corpo.appendChild(frame);
+  corpo.appendChild(frm);
 }
-NewsFrame();
+NewsFrame("frame", "div_a");
